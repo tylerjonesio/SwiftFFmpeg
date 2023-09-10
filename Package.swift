ae0b06db8de03c9b072943335b1541b0741d1b75
Tyler Jones <tyler@tylerjones.io>
2023-09-10 00:04:35 -0600

    Bump for 5.1.2.5

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/tylerjonesio/ffmpeg-kit-spm", branch: "min.v5.1.2.4")
+    .package(url: "https://github.com/tylerjonesio/ffmpeg-kit-spm", branch: "min.v5.1.2.5")
   ],
   targets: [
     .target(
